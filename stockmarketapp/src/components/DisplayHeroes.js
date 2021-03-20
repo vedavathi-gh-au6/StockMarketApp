42f79f1f8aa04c052df01f57906ce4f3c6d0e9d3
Vedavathi GH <vedavathi.gh@gmail.com>
2021-03-21 01:54:11 +0530

    initial commit

--- stockmarketapp/src/components/DisplayHeroes.js
+++ stockmarketapp/src/components/DisplayHeroes.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Card ,Button,Image} from 'react-bootstrap'
+import {Card ,Button,Image,Col,Row} from 'react-bootstrap'
 const DisplayHeroes = ({name,image}) => {
     return (
         <div className="container-fluid d-flex justify-content-center">
-           <div className='col-md-4'>
+           <Col xs={6} md={4}>
                 <Card.Title>{name}</Card.Title>
                 <Image src={image} alt="image" roundedCircle />
-                </div>
+                </Col>
                 </div>
                 
            
